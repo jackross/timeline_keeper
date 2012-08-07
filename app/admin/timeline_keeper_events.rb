dabb88319dc84ba2be8d770da9b346c657464a40
Jason Ronallo <jronallo@gmail.com>
2012-08-07 13:56:34 -0400

    include multiselect in form for choosing timelines

--- app/admin/timeline_keeper_events.rb
+++ app/admin/timeline_keeper_events.rb
@@ -1,3 +1,8 @@
 ActiveAdmin.register TimelineKeeper::Event do
-  
+  form do |f|
+    f.inputs :headline, :startDate, :endDate, :text, :media, :credit, :caption, 
+      :thumbnail, :timelines
+
+    f.buttons
+  end
 end
